cbe1946378f11ac83be5a3f01a74097d73b391f7
ziya <644735414@qq.com>
2021-10-20 08:15:59 -0700

    c string malloc

--- share/vm/utilities/hashMap.hpp
+++ share/vm/utilities/hashMap.hpp
@@ -15,13 +15,24 @@
 using std::size_t;
 using std::map;
 
+// overwrite map operator new function --> allocate map memory in metaspace
+// the operation of allocate memory in map, is can't allocate in metaspace
+template<class K, class V>
+class Map : public map<K, V> {
+public:
+    void* operator new (size_t size) {
+        return Metaspace::allocate(size)->value();
+    }
+    void operator delete(void* p) {}
+};
+
 template <class K, class V, class HashFn = HashCode<K>>
 class Hashmap : public MetaspaceObj {
 private:
     const size_t upperTol = 10;
     const size_t lowerTol = 2;
     const size_t initCapacity = 7;
-    Array<map<K, V>*>* _hashtable; // map<K, V>* _hashtable[1];
+    Array<Map<K, V>*>* _hashtable; // map<K, V>* _hashtable[1];
     size_t _size;
     size_t  _M;
 
@@ -52,9 +63,9 @@ Hashmap<K, V>* Hashmap<K, V, HashFn>::operator=(const Hashmap<K, V> *hashMap) {
 
     _hashtable = new Array<map<K, V>*>(hashMap->_M);
     for (int i = 0 ; i < hashMap->_M; ++i) {
-        map<K, V>* m = hashMap->get(i);
+        Map<K, V>* m = hashMap->get(i);
         for(auto iterator = m->begin(); iterator != m->end(); iterator++) {
-            map<string, string>* newMap = _hashtable->get(i);
+            Map<string, string>* newMap = _hashtable->get(i);
             (*newMap)[iterator->first] = iterator->second;
         }
     }
@@ -72,11 +83,11 @@ Hashmap<K, V, HashFn>::~Hashmap() {
 
 template<class K, class V, class HashFn >
 Hashmap<K, V, HashFn>::Hashmap(const Hashmap<K, V> *hashMap) {
-    _hashtable = new (hashMap->_M) Array<map<K, V>*>(hashMap->_M);
+    _hashtable = new (hashMap->_M) Array<Map<K, V>*>(hashMap->_M);
     for (int i = 0 ; i < hashMap->_M; ++i) {
-        map<K, V>* m = hashMap->get(i);
+        Map<K, V>* m = hashMap->get(i);
         for(auto iterator = m->begin(); iterator != m->end(); iterator++) {
-            map<string, string>* newMap = _hashtable->get(i);
+            Map<string, string>* newMap = _hashtable->get(i);
             (*newMap)[iterator->first] = iterator->second;
         }
     }
@@ -84,18 +95,18 @@ Hashmap<K, V, HashFn>::Hashmap(const Hashmap<K, V> *hashMap) {
 
 template<class K, class V, class HashFn >
 Hashmap<K, V, HashFn>::Hashmap(size_t M): _M(M), _size(0) {
-    _hashtable = new (M) Array<map<K, V>*>(M);
+    _hashtable = new (M) Array<Map<K, V>*>(M);
     for (int i = 0 ; i < M ; ++i) {
-        map<K, V>* _map = new map<K, V>();
+        Map<K, V>* _map = new Map<K, V>();
         _hashtable->insert(i, _map);
     }
 }
 
 template<class K, class V, class HashFn >
 Hashmap<K, V, HashFn>::Hashmap(): _M(initCapacity), _size(0) {
-    _hashtable = new (initCapacity) Array<map<K, V>*>(initCapacity);
+    _hashtable = new (initCapacity) Array<Map<K, V>*>(initCapacity);
     for (int i = 0 ; i < initCapacity ; ++i) {
-        _hashtable->insert(i, new map<K, V>());
+        _hashtable->insert(i, new Map<K, V>());
     }
 }
 
@@ -112,7 +123,7 @@ size_t Hashmap<K, V, HashFn>::size() {
 template<class K, class V, class HashFn>
 void Hashmap<K, V, HashFn>::put(K key, V value) {
     size_t i = _hash(key);
-    map<K, V>* m = _hashtable->get(i);
+    Map<K, V>* m = _hashtable->get(i);
 
     if (m->find(key) != m->end()) {
         (*m)[key] = value;
@@ -129,7 +140,7 @@ void Hashmap<K, V, HashFn>::put(K key, V value) {
 // get前需要使用contains判断是否存在，否则行为未定义
 template<class K, class V, class HashFn>
 V Hashmap<K, V, HashFn>::get(K key) {
-    map<K, V>* m = _hashtable->get(_hash(key));
+    Map<K, V>* m = _hashtable->get(_hash(key));
     if (m->find(key) != m->end()) {
         return (*m)[key];
     }
@@ -137,7 +148,7 @@ V Hashmap<K, V, HashFn>::get(K key) {
 
 template<class K, class V, class HashFn>
 V Hashmap<K, V, HashFn>::remove(K key) {
-    map<K, V>* m = _hashtable->get(_hash(key));
+    Map<K, V>* m = _hashtable->get(_hash(key));
     auto i = m->find(key);
     if (i != m->end()) {
         V ret = (*m)[key];
@@ -154,7 +165,7 @@ V Hashmap<K, V, HashFn>::remove(K key) {
 
 template<class K, class V, class HashFn>
 bool Hashmap<K, V, HashFn>::contains(K key) {
-    map<K, V>* m = _hashtable->get(_hash(key));
+    Map<K, V>* m = _hashtable->get(_hash(key));
     if (m->find(key) != m->end()) {
         return true;
     } else {
@@ -164,18 +175,18 @@ bool Hashmap<K, V, HashFn>::contains(K key) {
 
 template<class K, class V, class HashFn>
 void Hashmap<K, V, HashFn>::resize(size_t newM) {
-    Array<map<K, V>*>* newHashTable = new (newM) Array<map<K, V>*>(newM);
+    Array<Map<K, V>*>* newHashTable = new (newM) Array<Map<K, V>*>(newM);
     for (int i = 0 ; i < newM ; ++i) {
-        newHashTable->insert(i, new map<K, V>());
+        newHashTable->insert(i, new Map<K, V>());
     }
 
     size_t oldM = _M;
     _M = newM;
 
     for (int i = 0 ; i < oldM ; ++i) {
-        map<K, V>* m = _hashtable->get(i);
+        Map<K, V>* m = _hashtable->get(i);
         for(auto iterator = m->begin(); iterator != m->end(); iterator++) {
-            map<K, V>* newMap = newHashTable->get(_hash(iterator->first));
+            Map<K, V>* newMap = newHashTable->get(_hash(iterator->first));
             (*newMap)[iterator->first] = iterator->second;
         }
     }
